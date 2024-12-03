b8c5ea9cdce2bf9b34c435206981420de3e06f9d
Jack <giacomo.deluca@hotmail.it>
2024-12-03 15:02:13 +0100

    Storing color scheme with manager

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
 
-
-
 export default function App() {
   return (
+    <>
+    <ColorSchemeScript />
     <MantineProvider theme={theme} 
+    
     // defaultColorScheme="light"
     >
       <Router />
     </MantineProvider>
+  );
+  </>
   );
 }
