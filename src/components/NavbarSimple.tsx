38b61f2181387e093a9ccba8c4e0a22a538f983e
Jack <giacomo.deluca@hotmail.it>
2024-12-24 17:32:41 +0100

    cleaned up navbar

--- src/components/NavbarSimple.tsx
+++ src/components/NavbarSimple.tsx
@@ -161,17 +161,6 @@ export function NavbarSimple({
         {links} {/* Links from the original NavbarSimple */}
       </div>
 
-      <div className={classes.footer}>
-        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
-          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
-          <span>Change account</span>
-        </a>
-
-        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
-          <IconLogout className={classes.linkIcon} stroke={1.5} />
-          <span>Logout</span>
-        </a>
-      </div>
     </nav>
     
   );
