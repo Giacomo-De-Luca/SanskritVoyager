d4dfe94293175eb0b6451dd1a797090202d3da3d
Jack <giacomo.deluca@hotmail.it>
2024-12-26 14:06:44 +0100

    reverted to default mantine color manager to test flickering

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,17 @@ const colorSchemeManager = localStorageColorSchemeManager({
   key: 'mantine-color-scheme',
 });
 
-const colorSchemeScript = `
-  try {
-    var colorScheme = localStorage.getItem('mantine-color-scheme');
-    if (colorScheme) {
-      document.documentElement.setAttribute('data-mantine-color-scheme', colorScheme);
-    }
-  } catch (e) {}
-`;
 
 
 export default function App() {
   return (
     <>
     
-    <script dangerouslySetInnerHTML={{ __html: colorSchemeScript }} />
+    <script  />
       
       <MantineProvider
         theme={theme}
-        defaultColorScheme="light"
-        colorSchemeManager={localStorageColorSchemeManager({
-          key: 'mantine-color-scheme'
-
-        })}
+        colorSchemeManager={colorSchemeManager}
         
       >
       <Router />
